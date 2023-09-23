506c6758037ca4ef18b59b914763f94a9dff020e
akash krishna <akashgangan2001@gmail.com>
2023-09-23 20:56:55 +0530

    added more features

--- client/src/components/user/Userideas.jsx
+++ client/src/components/user/Userideas.jsx
@@ -93,7 +93,7 @@ function Userideas() {
                     <button
                       className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded "
                       onClick={() => {
-                        deletedata(row.studentName);
+                        deletedata(row.orderNO);
                       }}
                     >
                       Delete
